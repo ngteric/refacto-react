06efb38dd8e8eaa05b3e271f9a62fb3e4db00f4c
ngteric <nongeric@gmail.com>
2022-03-21 11:55:40 +0100

    refacto(domain-filter): remove state logic code from component + use selectors into container

--- src/components/DomainFilter/DomainFilter.container.ts
+++ src/components/DomainFilter/DomainFilter.container.ts
@@ -1,10 +1,12 @@
 import DomainFilter from './DomainFilter.component';
 import { connect } from 'react-redux'
-import { getDomains } from '../../redux/domains/selectors';
+import { getClassifications, getCountries, getSubClassifications } from '../../redux/domains/selectors';
 import { AppState } from '../../redux/store';
 
 const mapStateToProps = (state: AppState) => ({
-  domains: getDomains(state)
+  countries: getCountries(state),
+  classifications: getClassifications(state),
+  subClassifications: getSubClassifications(state),
 })
 
 export default connect(mapStateToProps)(DomainFilter)
